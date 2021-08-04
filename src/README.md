2d88f615b27c55977ae6b0277a114888b0657882
yifan zhuo <erfanvv@163.com>
2021-08-04 17:14:02 +0800

    归并排序的应用

---
+++ src/README.md
@@ -0,0 +1,4 @@
+### 排序
+- [归并排序](./sort/mergeSort.js)
+### 二分搜索
+- [在排序数组中查找元素的第一个和最后一个位置](./binarySearch/code_34.js)
---
+++ src/sort/sumOfLowerNumber.js
@@ -0,0 +1,66 @@
+/**
+ * 求一个数组中每个数的左侧小于该数的元素之和——用归并排序解决
+ * @param arr
+ * @param low
+ * @param high
+ */
+function sumOfLowerNumber(arr, low, high) {
+  let sum = 0;
+  /**
+   * 归并排序
+   * @param arr
+   * @param low
+   * @param high
+   */
+  const mergeSort = function (arr, low, high) {
+    if (low === high) {
+      return;
+    }
+    let mid = low + ((high - low) >> 1);
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, mid + 1, high);
+  }
+  /**
+   * 合并的时候，要算小和
+   * @param arr
+   * @param startOfLeftArr
+   * @param endOfLeftArr
+   * @param startOfRightArr
+   * @param endOfRightArr
+   */
+  const merge = function (arr, startOfLeftArr, endOfLeftArr, startOfRightArr, endOfRightArr) {
+    let num = startOfLeftArr;
+    let mergedArr = [];
+    while (startOfLeftArr <= endOfLeftArr && startOfRightArr <= endOfRightArr) {
+      if (arr[startOfLeftArr] <= startOfRightArr) {
+        sum += arr[startOfLeftArr] * (endOfRightArr - startOfRightArr + 1);
+        mergedArr.push(arr[startOfLeftArr]);
+        startOfLeftArr++;
+      } else {
+        mergedArr.push(startOfRightArr);
+        startOfRightArr++;
+      }
+    }
+    while (startOfLeftArr <= endOfLeftArr) {
+      mergedArr.push(arr[startOfLeftArr]);
+      startOfLeftArr++;
+    }
+    while (startOfRightArr <= endOfRightArr) {
+      mergedArr.push(arr[startOfRightArr]);
+      startOfRightArr++;
+    }
+    for (let e of mergedArr) {
+      arr[num++] = e;
+    }
+  }
+  mergeSort(arr, low, high);
+  return sum;
+}
+
+const test = function () {
+  let arr = [1, 3, 4, 2, 5];
+  let sum = sumOfLowerNumber(arr, 0, arr.length - 1);
+  console.log(sum);
+}
+test();
